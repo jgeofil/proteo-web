1a8431fa121bc89574125703697adb1d8f9f9cbb
jgeofil <jeremy.geo@gmail.com>
2016-01-11 15:21:05 -0500

    cleanup

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -6,24 +6,4 @@ describe('Controller: MainController', function() {
   beforeEach(module('proteoWebApp'));
   beforeEach(module('socketMock'));
 
-  var scope;
-  var MainController;
-  var $httpBackend;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function(_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
-
-    scope = $rootScope.$new();
-    MainController = $controller('MainController', {
-      $scope: scope
-    });
-  }));
-
-  it('should attach a list of things to the controller', function() {
-    $httpBackend.flush();
-    expect(MainController.awesomeThings.length).toBe(4);
-  });
 });
