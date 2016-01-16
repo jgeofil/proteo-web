f62ffb9e5925321dc501b701243228f485e1970b
jgeofil <jeremy.geo@gmail.com>
2016-01-16 11:12:32 -0500

    add labels to graph

--- client/app/orf/orf.controller.js
+++ client/app/orf/orf.controller.js
@@ -10,6 +10,7 @@ angular.module('proteoWebApp')
     $http.get('/api/data/' + $routeParams.datasetName + '/orf/' +
       $routeParams.orfName + '/analysis/disopred3')
     .then(function(data){
+      var inDat = data.data.data;
       //Protein sequence
       var diso3seq = new Sequence(data.data.seq);
       diso3seq.render('#sequence-viewer', {
@@ -25,7 +26,7 @@ angular.module('proteoWebApp')
 
       var coverage = [];
 
-      data.data.data.forEach(function(d){
+      inDat.forEach(function(d){
         var style = {start: d.pos-1, end: d.pos, color: 'black', underscore: false};
         if(d.bind.symbol === '^'){
           style.underscore = true;
@@ -44,51 +45,65 @@ angular.module('proteoWebApp')
       ];
       diso3seq.addLegend(exempleLegend);
 
-      var margin = {top: 20, right: 20, bottom: 30, left: 50},
-          width = 600 - margin.left - margin.right,
-          height = 200 - margin.top - margin.bottom;
+      var margin = {top: 20, right: 20, bottom: 50, left: 60},
+        width = 600 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
 
       var x = d3.scale.linear()
-          .range([0, width]);
+        .range([0, width]);
 
       var y = d3.scale.linear()
-          .range([height,0]);
+        .range([height,0]);
 
       var xAxis = d3.svg.axis()
-          .scale(x)
-          .orient('bottom');
+        .scale(x)
+        .orient('bottom');
 
       var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient('left')
-          .ticks(5);
-
+        .scale(y)
+        .orient('left')
+        .ticks(5);
 
       var line = d3.svg.line()
-          .x(function(d) { return x(d.pos); })
-          .y(function(d) { return y(d.diso.value); });
+        .x(function(d) { return x(d.pos); })
+        .y(function(d) { return y(d.diso.value); });
 
       var svg = d3.select('#diso-graph').append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
         .append('g')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-      x.domain(d3.extent(data.data.data, function(d) { return d.pos; }));
-      y.domain(d3.extent(data.data.data, function(d) { return d.diso.value===null?0:d.diso.value; }));
-
-      svg.append('g')
-          .attr('class', 'x axis')
-          .attr('transform', 'translate(0,' + height + ')')
-          .call(xAxis);
+      x.domain(d3.extent(inDat, function(d) { return d.pos; }));
+      y.domain(d3.extent(inDat, function(d) { return d.diso.value===null?0:d.diso.value; }));
 
       svg.append('g')
-          .attr('class', 'y axis')
-          .call(yAxis);
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+
+      svg.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis);
+
+      svg.append('text')
+        .attr('class', 'x label')
+        .attr('text-anchor', 'middle')
+        .attr('x', width/2)
+        .attr('y', height + 35)
+        .text('position');
+
+      svg.append('text')
+        .attr('class', 'y label')
+        .attr('text-anchor', 'middle')
+        .attr('y', -40)
+        .attr('x', -height/2)
+        .attr('transform', 'rotate(-90)')
+        .text('probability score');
 
       svg.append('path')
-          .attr('class', 'line')
-          .attr('d', line(data.data.data));
+        .attr('class', 'line')
+        .attr('d', line(inDat));
     });
 
     //**************************************************************************
@@ -104,7 +119,7 @@ angular.module('proteoWebApp')
           $routeParams.orfName + '/analysis/itasser/models/'+model)
         .then(function(data){
 
-          // Assume there exists an HTML div with id "gldiv"
+          // Assume there exists an HTML div with id 'gldiv'
           var element = $('#gldiv-'+model);
 
           // Viewer config - properties 'defaultcolors' and 'callback'
