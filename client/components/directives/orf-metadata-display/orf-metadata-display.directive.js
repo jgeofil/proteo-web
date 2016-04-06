a47598816b9035998f69080b2c3687947974d49e
jgeofil <jeremy.geo@gmail.com>
2016-04-06 12:23:03 -0400

    sol

--- client/components/directives/orf-metadata-display/orf-metadata-display.directive.js
+++ client/components/directives/orf-metadata-display/orf-metadata-display.directive.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* jshint undef: false*/
 angular.module('proteoWebApp')
   .directive('orfMetadataDisplay', function (d3Helper) {
     return {
@@ -9,67 +9,64 @@ angular.module('proteoWebApp')
         mdMetadata: '='
       },
       link: function (scope, element, attrs) {
-        var width = 80;
-        var height = 100;
 
         // Size and margins
+        var width = 120;
+        var height = 70;
         var svg = d3.select('#orf-solubility').append('svg')
           .attr('width', width)
           .attr('height', height)
           .append('g')
-          .attr('transform', 'translate(45,10)');
-
-        // Scales and domains
-        var y = d3.scale.linear().range([height-20,0]);
-        y.domain([0,1]);
+          .attr('transform', 'translate(10,0)');
 
         //**********************************************************************
-        // Axes
-        var yAxis = d3.svg.axis().scale(y).orient('left').ticks(3);
+        // Scales and domains
+        var y = d3.scale.linear().range([0,width-20]);
+        y.domain([0,1]);
 
+        // Axes
+        var yAxis = d3.svg.axis().scale(y).orient('bottom').ticks(3);
         svg.append('g')
           .attr('class', 'y axis')
-          .call(yAxis);
+          .call(yAxis)
+          .attr('transform', 'translate(0,20)');
 
-        var gradient = svg.append('defs')
-          .append('linearGradient')
-            .attr('id', 'gradient2')
-            .attr('x1', '0%')
-            .attr('y1', '0%')
-            .attr('x2', '100%')
-            .attr('y2', '100%')
-            .attr('spreadMethod', 'pad');
-
-        gradient.append('stop')
-            .attr('offset', '0%')
-            .attr('stop-color', '#0c0')
-            .attr('stop-opacity', 1);
-
-        gradient.append('stop')
-            .attr('offset', '100%')
-            .attr('stop-color', '#c00')
-            .attr('stop-opacity', 1);
-
-            svg.append("circle")
-                         .attr("cx", 5)
-                         .attr("cy", 10)
-                         .attr("r", 2).style("fill", "red")
-       svg.append('text')
-         .attr('class', 'y label')
-         .attr('text-anchor', 'middle')
-         .attr('y', 13)
-         .attr('x', 20)
-         .text('0.83')
-         .style("fill", "red")
-
+        //**********************************************************************
+        // Value circle and label
         svg.append('text')
           .attr('class', 'y label')
           .attr('text-anchor', 'middle')
-          .attr('y', -35)
-          .attr('x', -(height-20)/2)
-          .attr('transform', 'rotate(-90)')
-          .text('solubility');
+          .attr('y', 10)
+          .attr('x', (width-20)/2)
+          .attr('id', 'unset-label')
+          .text('Unset')
+          .style('fill', 'red');
 
-      }
-    };
+        scope.$watch('mdMetadata.solubility',function(newValue, oldValue){
+          if(newValue !== oldValue){
+            d3.select('#unset-label').remove();
+            svg.append('circle')
+              .attr('cx', y(scope.mdMetadata.solubility))
+              .attr('cy', 15)
+              .attr('r', 2)
+              .style('fill', 'red');
+
+            svg.append('text')
+              .attr('class', 'y label')
+              .attr('text-anchor', 'middle')
+              .attr('y', 10)
+              .attr('x', y(scope.mdMetadata.solubility))
+              .text(scope.mdMetadata.solubility)
+              .style('fill', 'red');
+          }
+        });
+
+        // Value label
+        svg.append('text')
+          .attr('class', 'y label')
+          .attr('text-anchor', 'middle')
+          .attr('y', 55)
+          .attr('x', (width-20)/2)
+          .text('solubility');
+    }};
   });
