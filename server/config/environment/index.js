cd95c6f5739f3d1c0fe7a8df14535ec10f63b781
Jeremy <jeremy.geo@gmail.com>
2016-08-22 19:44:25 -0400

    Update index.js

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -25,7 +25,7 @@ var all = {
   ip: process.env.IP || '0.0.0.0',
 
   // Should we populate the DB with sample data?
-  seedDB: false,
+  seedDB: true,
 
   // Data location
   data: process.env.DATA_LOC || path.normalize(__dirname + '/../../data/'),
